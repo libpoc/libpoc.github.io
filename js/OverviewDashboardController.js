da0ff19c61ae82680e5ef396bde04868bcac2be9
jeffeld <eldridge.jeff@gmail.com>
2014-03-28 18:37:06 +0000

    Overview dashboard

---
+++ js/OverviewDashboardController.js
@@ -0,0 +1,19 @@
+function OverviewDashboardController ($scope) {
+
+
+    $scope.Overview = {
+        stock : {
+            total : 10324,
+            out : 2843,
+            overdue : 43
+        },
+
+        today : {
+            out : 17,
+            in : 24,
+            due : 67
+        }
+    }
+
+
+}
