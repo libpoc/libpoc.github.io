aee395426131e74e58a15ea62f8f88d3cab6c2c3
Jeff Eldridge <eldridge.jeff@gmail.com>
2014-03-31 12:29:52 +0100

    Made overview live

--- js/OverviewDashboardController.js
+++ js/OverviewDashboardController.js
@@ -1,54 +1,78 @@
-function OverviewDashboardController ($scope) {
-
+function OverviewDashboardController ($scope, $timeout) {
 
     $scope.Overview = {
         stock : {
-            total : 10324,
-            out : 2843,
-            overdue : 43
+            total : _.random(5000, 10000),
+            out : _.random(500,1000),
+            overdue : _.random (5,20)
         },
 
         today : {
-            out : 17,
-            in : 24,
-            due : 67
+            out : 0,
+            in : 0,
+            due : _.random (3,33)
         }
-    }
-
-    var i = 0;
-    $scope.Last30Days = [];
-    for (i = 0; i <= 30; i++) {
-        $scope.Last30Days.push ({
-            date : new Date() - i,
-            loans : _.random (20,60)
-        });
-    }
+    };
 
     var last30Days = [
         {
             "key" : "Loans",
-            "values" : [[0, 34], [-1, 22],[-2, 56],[-3, 32],[-4, 37],[-5, 51],[-6, 19],[-7, 27],[-8, 31],[-9, 25],[-10, 57]]
+            "values" : []
         },
         {
             "key" : "Returns",
-            "values" : [[0, 7], [-1, 13],[-2, 18],[-3, 44],[-4, 32],[-5, 1],[-6, 35],[-7, 7],[-8, 9],[-9, 22],[-10, 46]]
+            "values" : []
         },
         {
             "key" : "Overdues",
-            "values" : [[0, 3], [-1, 5],[-2, 7],[-3, 2],[-4, 1],[-5, 0],[-6, 0],[-7, 0],[-8, 3],[-9, 5],[-10, 3]]
+            "values" : []
         }
-
-
     ];
+
+    var now = new Date(Date.now()), dd={};
+    for (var i = 1; i <= 30; i++) {
+        dd = new Date().setDate (now.getDate() - i);
+        last30Days[0].values.push ([new Date(dd), _.random(20,60)]);
+        last30Days[1].values.push ([new Date(dd), _.random(5,33)]);
+        last30Days[2].values.push ([new Date(dd), _.random(0,7)]);
+    }
+
     $scope.Last30Days = last30Days;
 
     $scope.xAxisTickFormat = function(){
         return function(d){
 //                    return d3.time.format('%X')(new Date(d));  //uncomment for time format
-            return d3.time.format('%x')(new Date(d + Date.now()));  //uncomment for date format
+            return d3.time.format('%x')(new Date(d));  //uncomment for date format
         }
     }
 
+    updateLoans = function () {
+        var r = _.random(1,4);
+        $scope.Overview.today.out += r;
+        $scope.Overview.stock.out += r;
+        $timeout (updateLoans, _.random(3,15)*1000);
+    }
 
+    updateIns = function () {
+        var t = _.random(1,4);
+        $scope.Overview.today.in += t;
+        $scope.Overview.stock.out -= t;
+
+        if (t == 1) {
+            $scope.Overview.stock.overdue = Math.max ($scope.Overview.stock.overdue - t, 0);
+
+            if ($scope.Overview.stock.overdue === 0) {
+                OverviewDashboardController($scope, $timeout);
+            }
+
+
+        }
+
+        $timeout (updateIns, _.random(5,20)*1000);
+    }
+
+    $timeout (updateLoans, _.random(3,15)*1000);
+    $timeout (updateIns, _.random(5,20)*1000);
 
 }
+
